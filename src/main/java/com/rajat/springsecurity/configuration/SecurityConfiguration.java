8853001b929ad08112f094142cc1b8fac31f7eab
Rajat <tyagirajat005@gmail.com>
2024-08-17 02:13:16 +0530

    custom configuration

---
+++ src/main/java/com/rajat/springsecurity/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.rajat.springsecurity.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(customizer->customizer.disable())
+                .authorizeHttpRequests(request->request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
--- src/main/java/com/rajat/springsecurity/controller/HelloController.java
+++ src/main/java/com/rajat/springsecurity/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.rajat.springsecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 @GetMapping("/")
-    public String greet() {
-        return "Welcome to Spring security Rajat";
+    public String greet(HttpServletRequest httpServletRequest) {
+        return "Welcome to Spring security Rajat" + httpServletRequest.getSession().getId();
     }
 }
